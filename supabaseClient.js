295ad395fda0d50d3ff67ea22660596584fbd773
iisacbernardi <160758186+iisacbernardi@users.noreply.github.com>
2025-05-12 12:20:41 -0300

    lsl

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,6 +3,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
-  'https://wqrvadycionozczmqgoj.supabase.co',
+  'https://ohbpwiftmqnhextjmqpq.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxcnZhZHljaW9ub3pjem1xZ29qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0OTUzOTIsImV4cCI6MjA2MjA3MTM5Mn0.mMVAn7kJQH8bLhijfug2E0lMvpjRkKRE7CC29IFNNt4'
 )
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oYnB3aWZ0bXFuaGV4dGptcXBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NTc4MjYsImV4cCI6MjA2MjAzMzgyNn0.EKvg1YYE4qzbsE7Jc4xUqOaU9AiOQlfoHlOfJP6THjI
